54d77a739ffb8c9a3ffea02566f378d27b1a0384
Apocalipse <Apocalipse@t49061.unir.br>
2016-09-30 18:54:35 -0400

    .

---
+++ aprendendo-java2/src/main/java/com/ifnc/model/Usuario.java
@@ -0,0 +1,64 @@
+package com.ifnc.model;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1004942967100049692L;
+	
+	private long codigo;
+	private String nome;
+	private String sobrenome;
+	private String senha;
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (codigo ^ (codigo >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (codigo != other.codigo)
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
