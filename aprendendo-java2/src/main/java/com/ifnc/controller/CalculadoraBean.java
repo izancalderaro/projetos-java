b8c5735b9548c30de0e9014611fd2555ba5624a3
Apocalipse <Apocalipse@Apocalipse>
2016-10-18 23:55:45 -0300

    Implementado converter inline

--- aprendendo-java2/src/main/java/com/ifnc/controller/CalculadoraBean.java
+++ aprendendo-java2/src/main/java/com/ifnc/controller/CalculadoraBean.java
@@ -7,63 +7,63 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.ifnc.service.Calculadora;
-import com.ifnc.service.NotaFinal;
 import com.ifnc.service.PesoDois;
 
 @Named
 @RequestScoped
 public class CalculadoraBean {
 
-	@Inject @PesoDois
+	@Inject
+	@PesoDois
 	private Calculadora calculadora;
 
-	private int a,b;
-	
+	private float a, b;
+
 	private float resultado;
-	
+
 	@PostConstruct
 	public void init() {
-		a = 10;
-		b = 0;
+		a = 5;
+		b = 2;
 	}
-	
+
 	public void Calcular() {
-		
+
 		resultado = calculadora.soma(a, b);
-		
+
 		System.out.println(resultado);
 	}
 
-	
 	public Calculadora getCalculadora() {
 		return calculadora;
 	}
 
-		
 	public void setCalculadora(Calculadora calculadora) {
 		this.calculadora = calculadora;
 	}
 
-		
+	public float getA() {
+		return a;
+	}
+
+	public void setA(float a) {
+		this.a = a;
+	}
+
+	public float getB() {
+		return b;
+	}
+
+	public void setB(float b) {
+		this.b = b;
+	}
+
 	public float getResultado() {
 		return resultado;
 	}
 
-
-	public int getA() {
-		return a;
+	public void setResultado(float resultado) {
+		this.resultado = resultado;
 	}
-
-	public void setA(int a) {
-		this.a = a;
-	}
-
-	public int getB() {
-		return b;
-	}
-
-	public void setB(int b) {
-		this.b = b;
-	}
-
+	
 }
