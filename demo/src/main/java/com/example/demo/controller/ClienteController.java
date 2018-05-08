e904f7a179b98f0624dde53da5e4dd25b9102668
Apocalipse <Apocalipse@T49061.unir.br>
2018-05-08 17:26:47 -0400

    Incluido layout

--- demo/src/main/java/com/example/demo/controller/ClienteController.java
+++ demo/src/main/java/com/example/demo/controller/ClienteController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +31,14 @@ public class ClienteController {
 		return mv;
 	}
 	
+	@RequestMapping
+	public ModelAndView pesquisar() {
+            List<Cliente> todosClientes = clientes.findAll();
+            ModelAndView mv = new ModelAndView("PesquisaCliente");
+            mv.addObject("todosClientes", todosClientes);
+            return mv;
+		
+	}
 	
 	
 }
