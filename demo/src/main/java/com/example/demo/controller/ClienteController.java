2b84235859c58f18638658706431b5c6b4af10c0
Apocalipse <Apocalipse@T49061.unir.br>
2018-05-08 16:09:23 -0400

    Termino da inclusão

--- demo/src/main/java/com/example/demo/controller/ClienteController.java
+++ demo/src/main/java/com/example/demo/controller/ClienteController.java
@@ -1,16 +1,32 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.model.Cliente;
+import com.example.demo.repository.Clientes;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
 
+	@Autowired
+	private Clientes clientes;
+	
 	@RequestMapping("/novo")
 	public String novo() {
 		return "CadastroCliente";
 	}
 	 
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView salvar(Cliente cliente) {
+		clientes.save(cliente);
+		ModelAndView mv = new ModelAndView("CadastroCliente");
+		mv.addObject("mensagem" , "Cliente adicionado com sucesso");
+		return mv;
+	}
 	
 }
